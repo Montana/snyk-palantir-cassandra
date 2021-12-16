c3f598ffbe8315bb53cfe433b7fbb645879cff9f
Montana Mendy <montana@getprowl.com>
2021-12-15 18:15:23 -0800

    snyk terraform config/test

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "public_subnet_id_1" {
+  default = "This should be replaced in the primary main.tf"
+}
+
+variable "public_subnet_id_2" {
+  default = "This should be replaced in the primary main.tf"
+}
+
+variable "public_subnet_id_3" {
+  default = "This should be replaced in the primary main.tf"
+}
+
+variable "cluster_name" {
+  default = "This should be replaced in the primary main.tf"
+}
